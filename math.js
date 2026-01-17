eba0a1b4871ef8a7210d1721c22f657e6649229a
Stephen Austin <austin.stephen@gmail.com>
2026-01-17 05:47:24 -0800

    Added isEven function, normalized commenting format.

--- math.js
+++ math.js
@@ -1,9 +1,17 @@
-// Double - this simple function doubles a number
+// double - this simple function doubles a number
 function double(x) {
   return x * 2;
 };
 
-// Square - this simple function squares a number, or multiplies it by itself
+// square - this simple function squares a number, or multiplies it by itself
 function square(x) {
   return x * x;
 };
+
+// isEven - this simple function checks if a number is even and confirms or refutes
+function isEven(x) {
+  if (x % 2 === -0)
+    console.log("This number is even");
+  else
+    console.log("This number is either odd or not a number.");
+}
