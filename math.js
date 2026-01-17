8e5447f405f994fd6d0325c0df5635ae4f8bd2df
Stephen Austin <austin.stephen@gmail.com>
2026-01-17 04:55:49 -0800

    Added a second function, square, and added comments

--- math.js
+++ math.js
@@ -1,3 +1,9 @@
+// Double - this simple function doubles a number
 function double(x) {
   return x * 2;
+};
+
+// Square - this simple function squares a number, or multiplies it by itself
+function square(x) {
+  return x * x;
 };
